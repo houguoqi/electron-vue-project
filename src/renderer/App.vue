733e5ab2269df207cb579e4cfd0ccd994425b1e4
PVI\ly-houguoqi <2946061522@qq.com>
2021-01-30 13:48:52 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+      <el-button type="danger">测试element</el-button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-project'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
