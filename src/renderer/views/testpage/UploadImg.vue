170df97812edb6fd49e7b5ad7cbbd49c7216ad6f
PVI\ly-houguoqi <2946061522@qq.com>
2021-02-01 14:13:19 +0800

    初步完成批量压缩图片并写入自定文件夹内；

--- src/renderer/views/testpage/UploadImg.vue
+++ src/renderer/views/testpage/UploadImg.vue
@@ -11,16 +11,36 @@
       :on-remove="handleRemove">
       <i class="el-icon-plus"></i>
     </el-upload>
-    <el-col>
-      <el-button @click="imgDeal" size="small">压缩</el-button>
-    </el-col>
+    <el-row style="margin-top:15px;">
+      <el-col :span="6">
+        <el-input @input="chooseDir" v-model="outPutDirPath" size="small" placeholder="填写输出目录" />
+      </el-col>
+    </el-row>
+    <el-row style="margin-top:15px;">
+      <el-col :span="6">
+        <el-select style="width:100%;" size="small" v-model="optity" placeholder="请选择清晰度">
+          <el-option
+            v-for="item in optityArr"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-col>
+    </el-row>
+    <el-row style="margin-top:15px;">
+      <el-col :span="6">
+        <el-button style="width:100%;" @click="imgDeal" type="primary" size="small">开始压缩</el-button>
+      </el-col>
+    </el-row>
     <el-dialog :visible.sync="dialogVisible">
       <img width="100%" :src="dialogImageUrl" alt="">
     </el-dialog>
   </div>
 </template>
 <script>
-  var fs = require('fs');
+  const fs = require('fs');
+  const path = require('path');
   export default {
     data() {
       return {
@@ -30,22 +50,57 @@
         limit: 50,
         file: null,
         fileList: [],
-        compressFileList: []
+        compressFileList: [],
+        compressFileListBase64: [],
+        isFinish: false,
+        loading: null,
+        outPutDirPath: '',
+        optityArr: [{
+          label: '压缩50%',
+          value: 1
+        },{
+          label: '压缩适中',
+          value: 0.6
+        },{
+          label: '最大压缩',
+          value: 0.1
+        }],
+        optity: 0.6
       };
     },
     mounted() {
-      console.log(fs)
-      fs.stat('D:/BF/Desktop/新建文件夹', function (err, stats) {
-          if (err) {
-            console.log(err)
-            return
-          }
-          console.log(stats.isDirectory(), 'isDirectory')
-          console.log(stats.isFile(), 'isFile')
-          console.log(stats, 'stats');
-      })
+      if (window.localStorage.getItem('outPutDirPath')) {
+        this.outPutDirPath = window.localStorage.getItem('outPutDirPath')
+      }
+      // console.log(fs)
+      // const dirpath = "D:/BF/Desktop/新建文件夹/"
+      // fs.readdir(dirpath, function(err, files){
+      //   if (err) {
+      //       return console.error(err);
+      //   }
+      //   fs.mkdir("D:/BF/Desktop/新建文件夹/新目录",function(err){
+      //     if (err) {
+      //         return console.error(err);
+      //     }
+      //     console.log("目录创建成功。");
+      //   });
+      //   files.forEach( function (file){
+      //       console.log( file );
+      //       fs.stat(path.join(dirpath, file), function (error, result) {
+      //         console.log(result.isDirectory(), result.isFile())
+      //       })
+      //   });
+      // });
     },
     methods: {
+      // 设置输出目录
+      chooseDir(value) {
+        console.log(value)
+        this.outPutDirPath = value.replace(/\\/g,"/") + '/'
+        if (this.outPutDirPath) {
+          window.localStorage.setItem('outPutDirPath', this.outPutDirPath)
+        }
+      },
       uploadChange(file, fileList) {
         this.fileList = []
         if (fileList.length) {
@@ -62,21 +117,67 @@
         this.dialogVisible = true;
       },
       imgDeal() {
+        this.loading = null
+        this.isFinish = false
+        this.compressFileList = []
+        this.compressFileListBase64 = []
         if (this.fileList.length) {
-          const loading = this.$loading({
+          this.loading = this.$loading({
             text: '压缩中...'
           })
           this.fileList.forEach(ele => {
             this.file = ele
             this.imgCompress(ele)
           })
-          loading.close();
-          this.$message({
-            message: '压缩成功',
-            type: 'success'
-          });
-          console.log(this.compressFileList, 'compressFileList')
         }
+      },
+      getImageType(str) {
+        var reg = /\.(png|jpg|gif|jpeg|webp)$/;
+        return str.match(reg)[1];
+      },
+      // 将图片转为base64
+      ToBase64(files) {
+          const self = this;
+          /*转换base64*/
+          if (files.length) {
+            files.forEach(file => {
+              var imgFile = new FileReader();
+              imgFile.readAsDataURL(file);
+              imgFile.onload = function (e) {
+                  var imgData = e.target.result; //base64数据
+                  self.compressFileListBase64.push(imgData)
+                  if (self.compressFileListBase64.length === self.fileList.length) {
+                    self.isFinish = true
+                    self.loading.close();
+                    self.$message({
+                      message: '压缩成功, 开始写入',
+                      type: 'success'
+                    });
+                    self.startWrite()
+                  }
+              }
+            })
+          }
+      },
+      // 开始写入
+      startWrite() {
+        const self = this
+        self.compressFileListBase64.forEach((ele, index) => {
+          const base64Data = ele.replace(/^data:image\/\w+;base64,/, ""),
+          dataBuffer = Buffer.from(base64Data, 'base64'),
+          getName = self.fileList[index].name
+          fs.writeFile(self.outPutDirPath + getName, dataBuffer, err => {
+              if (err) {
+                  throw err;
+              };
+              console.log('写入成功')
+          });
+        })
+      },
+      // 写入压缩后的图片
+      writeCompressedImg(compressFileList) {
+        if (!compressFileList || !compressFileList.length) return console.log('没有收到压缩图片列表')
+        this.ToBase64(compressFileList)
       },
       // 处理图片
       imgCompress(file) {
@@ -102,6 +203,11 @@
                   img.onload = function() {
                       let data = self.compress(img)
                       self.compressFileList.push(self.dataURLtoFile(data, file))
+                      // 判断是否完成
+                      if (self.compressFileList.length === self.fileList.length) {
+                        console.log(self.compressFileList, 'compressFileList')
+                        self.writeCompressedImg(self.compressFileList)
+                      }
                   }
               }
           }
@@ -151,7 +257,7 @@
               ctx.drawImage(img, 0, 0, width, height)
           }
           //进行最小压缩
-          let ndata = canvas.toDataURL('image/jpeg', 0.1)
+          let ndata = canvas.toDataURL('image/jpeg', this.optity)
           tCanvas.width = tCanvas.height = canvas.width = canvas.height = 0;
           return ndata
       },
